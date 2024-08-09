8a783cc18e4e0f7d8411acadcada7815067e7032
0425kid <152267292+jun220@users.noreply.github.com>
2024-08-09 18:28:51 +0900

    Bumpercar Move Update

--- Assets/Scripts/TestCode/TempKartController.cs
+++ Assets/Scripts/TestCode/TempKartController.cs
@@ -1,59 +1,75 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using BumperCarProject.Car.SO;
+using BumperCarProject.UI.View;
+using BumperCarProject.UI.Presenter;
 
 public class TempKartController : KartControl {
+    [SerializeField]
+    private BumperCar _bumperCar;
 
-    public float MAX_MOVE_SPEED;
     public float ROT_SPEED;
     public float MOVE_ACCELERATION;
     public float MOVE_DECELERATION;
 
+    private readonly float _boostingTime = 6f;
+    private float _maxSpeed;
+    private float _acceleration;
+    private float _deceleration;
+
     public Rigidbody Rigidbody;
 
-    private float moveSpeed;
+    private float _speed;
+    public float Speed
+    {
+        get => _speed;
+        set
+        {
+            _speed = value;
+            DashboardView.presenter.UpdateCurSpeed(value);
+        }
+    }
 
     public Camera mainCamera;
 
-    private void Start()
+    [SerializeField]
+    private float _damage;
+    public float Damage
     {
-        SetCamera();
-    }
-
-    protected override void Move(KartInput.NetworkInputData input) { }
-
-    protected override void Accelate(KartInput.NetworkInputData input) {
-        if(input.GetButton(KartInput.NetworkInputData.ButtonType.ACCELERATION)) {
-            moveSpeed = Mathf.Lerp(moveSpeed, MAX_MOVE_SPEED, MOVE_ACCELERATION * DeltaTime);
-        } else if(input.GetButton(KartInput.NetworkInputData.ButtonType.REVERSE)) {
-            moveSpeed = Mathf.Lerp(moveSpeed, -MAX_MOVE_SPEED, MOVE_ACCELERATION * DeltaTime);
-        } else {
-            moveSpeed = Mathf.Lerp(moveSpeed, 0, MOVE_DECELERATION * DeltaTime);
+        get => _damage;
+        set
+        {
+            _damage = value;
+            DashboardView.presenter.UpdateCurDamage(value);
         }
-
-        Vector3 velocity = Rigidbody.rotation * Vector3.forward * moveSpeed + Vector3.up * Rigidbody.velocity.y;
-        Rigidbody.velocity = velocity;
     }
 
-    protected override void Steer(KartInput.NetworkInputData input) {
-        Quaternion rotation = Quaternion.Euler(
-            Vector3.Lerp(
-                Rigidbody.rotation.eulerAngles,
-                Rigidbody.rotation.eulerAngles + Vector3.up * ROT_SPEED * input.Steer,
-                Runner.DeltaTime
-            )
-        );
-
-        Rigidbody.MoveRotation(rotation);
+    private bool _canBoost;
+    private bool _isBoosting;
+    public bool IsBoosting
+    {
+        get => _isBoosting;
+        set
+        {
+            _isBoosting = value;
+            if (value)
+            {
+                _maxSpeed = _bumperCar.boosterMaxSpeed;
+                _acceleration *= 1.2f;
+                _canBoost = false;
+                //StopBoostingAsync().Forget();
+            }
+            else
+            {
+                _maxSpeed = _bumperCar.maxSpeed;
+                _acceleration = _bumperCar.acceleration;
+            }
+        }
     }
 
-    protected override void Dash() { }
+    private float targetSpeed;
 
-    protected override void CollisionEnter(GameObject other) { }
-
-    protected override void CollisionExit(GameObject other) { }
-
-    protected override void CollisionStay(GameObject other) { }
 
     public void SetCamera()
     {
@@ -78,4 +94,85 @@ public class TempKartController : KartControl {
             Debug.LogWarning("Main Camera not found!");
         }
     }
+
+    public void Set()
+    {
+        SetCamera();
+
+        _canBoost = true;
+        _acceleration = _bumperCar.acceleration;
+        _deceleration = _bumperCar.deceleration;
+        _maxSpeed = _bumperCar.maxSpeed;
+    }
+
+    #region MonoBehaviour
+    private void Start()
+    {
+        Set();
+
+    }
+    #endregion
+
+
+    #region Override
+
+
+
+    protected override void Move(KartInput.NetworkInputData input) { }
+
+    protected override void Accelate(KartInput.NetworkInputData input) {
+
+        //if(input.GetButton(KartInput.NetworkInputData.ButtonType.ACCELERATION)) {
+        //    targetSpeed = Mathf.Lerp(Speed, _maxSpeed, _acceleration * DeltaTime);
+        //} else
+        //{
+        //    if (input.GetButton(KartInput.NetworkInputData.ButtonType.REVERSE))
+        //    {
+        //        targetSpeed = Mathf.Lerp(Speed, -_maxSpeed, _acceleration * DeltaTime);
+        //    }
+        //    targetSpeed = Mathf.Lerp(Speed, 0, MOVE_DECELERATION * DeltaTime);
+        //}
+
+        if (input.GetButton(KartInput.NetworkInputData.ButtonType.ACCELERATION))
+        {
+            targetSpeed = Mathf.Lerp(Speed, _maxSpeed, _acceleration * DeltaTime);
+        }
+        else if (input.GetButton(KartInput.NetworkInputData.ButtonType.REVERSE))
+        {
+            targetSpeed = Mathf.Lerp(Speed, -_maxSpeed, _acceleration * DeltaTime);
+        }
+        else
+        {
+            targetSpeed = Mathf.Lerp(Speed, 0, MOVE_DECELERATION * DeltaTime);
+        }
+
+
+        Vector3 velocity = Rigidbody.rotation * Vector3.forward * targetSpeed + Vector3.up * Rigidbody.velocity.y;
+        Rigidbody.velocity = velocity;
+        Speed = velocity.magnitude;
+    }
+
+    protected override void Steer(KartInput.NetworkInputData input) {
+        Quaternion rotation = Quaternion.Euler(
+            Vector3.Lerp(
+                Rigidbody.rotation.eulerAngles,
+                Rigidbody.rotation.eulerAngles + Vector3.up * ROT_SPEED * input.Steer,
+                Runner.DeltaTime
+            )
+        );
+
+        Rigidbody.MoveRotation(rotation);
+    }
+
+    protected override void Dash() { }
+
+    protected override void CollisionEnter(GameObject other) { }
+
+    protected override void CollisionExit(GameObject other) { }
+
+    protected override void CollisionStay(GameObject other) { }
+
+
+
+    #endregion
 }
